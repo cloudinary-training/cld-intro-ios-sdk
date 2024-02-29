3116c8c0cb7ab79f2045ddb6712f1290d0429188
adimiz1 <adi.mizrahi@cloudinary.com>
2024-02-29 08:41:42 +0200

    Add project files

---
+++ Cloudinary_Academy_App/Controllers/Widgets/UploadWidgetViewController.swift
@@ -0,0 +1,64 @@
+//
+//  UploadWidgetViewController.swift
+//  Cloudinary_Academy_App
+//
+//  Created by Adi Mizrahi on 19/09/2023.
+//
+
+import Foundation
+import UIKit
+import Cloudinary
+
+class UploadWidgetViewController: UIViewController {
+    
+    @IBOutlet weak var ivMain: CLDUIImageView!
+    @IBOutlet weak var vwOpenGallery: UIView!
+    
+    var cloudinary = CloudinaryHelper.shared.cloudinary
+    var uploadWidget: CLDUploaderWidget!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUploadButton()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    func setUploadButton() {
+        vwOpenGallery.layer.cornerRadius = vwOpenGallery.frame.height / 2
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(openUploadWidget))
+        vwOpenGallery.addGestureRecognizer(tapGesture)
+    }
+
+    @objc func openUploadWidget() {
+        let configuration = CLDWidgetConfiguration(
+          uploadType: CLDUploadType(signed: false, preset: "ios_sample"))
+
+        uploadWidget = CLDUploaderWidget(
+          cloudinary: cloudinary,
+          configuration: configuration,
+          images: nil,
+          delegate: self)
+
+        uploadWidget.presentWidget(from: self)
+    }
+
+
+    @objc func stepBack() {
+        self.dismiss(animated: true)
+    }
+}
+
+extension UploadWidgetViewController: CLDUploaderWidgetDelegate {
+    func uploadWidget(_ widget: CLDUploaderWidget, willCall uploadRequests: [CLDUploadRequest]) {
+      uploadRequests[0].response( { response, error in
+          self.ivMain.cldSetImage(response!.secureUrl!, cloudinary: self.cloudinary)
+      } )
+    }
+    func widgetDidCancel(_ widget: CLDUploaderWidget) {
+    }
+    func uploadWidgetDidDismiss() {
+    }
+}
